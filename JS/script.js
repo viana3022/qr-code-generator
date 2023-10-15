0143e25615bbe7719e2349bff0f2771b237a0d7e
viana3022 <vianavinicius444@gmail.com>
2023-10-15 14:25:18 -0300

    Implementação funcionalidade

---
+++ JS/script.js
@@ -0,0 +1,4 @@
+const container = document.querySelector(".container");
+const userInput = document.getElementById("userInput");
+const submitBtn = document.getElementById("submit");
+const downloadBtn = document.getElementById("download");
